3d0a2928fed383840d5381e19720b8aa64bfac29
Paopun20 <125128258+Paopun20@users.noreply.github.com>
2026-04-27 18:18:30 +0700

    one more fix

--- tests/tests/TestSpeed.hx
+++ tests/tests/TestSpeed.hx
@@ -2,23 +2,41 @@ package tests.tests;
 
 import paopao.hython.*;
 import tests.unit.TestCase;
-
 import haxe.Timer;
 
 class TestSpeed extends TestCase {
-    public function testSpeed1() {
-        var vm = new VM();
-        var start = Timer.stamp();
+	public function testSpeedForLoop() {
+		var vm = new VM();
+		var start = Timer.stamp();
 
-        executeInto(vm, "
+		executeInto(vm, "
 x = 0
 for i in range(1000000):
     x = x + i
 ");
 
-        var end = Timer.stamp();
-        trace("Time: " + (end - start) + " seconds");
-    }
+		var end = Timer.stamp();
+		trace("Time: " + (end - start) + " seconds");
+	}
+
+	public function testSpeedX() {
+		var vm = new VM();
+		var start = Timer.stamp();
+
+		var source = "
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+result = fib(25)
+";
+
+		executeInto(vm, source);
+
+		var end = Timer.stamp();
+		trace("Time: " + (end - start) + " seconds");
+	}
 
 	private function executeInto(vm:VM, source:String):Void {
 		var lexer = new Lexer(source);
